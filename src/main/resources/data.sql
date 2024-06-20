ab744bb33715aa711dc493e19986c6471cf4b0bd
DragonSlayer <varattleff@Viktors-MacBook-Air.local>
2024-06-20 10:56:21 +0200

    create moch test data in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,19 @@ VALUES ('John', 25),
        ('Jane', 30),
        ('Doe', 35);
 
+
+INSERT INTO participant (full_name, gender, age, adjacent_club, age_group)
+VALUES ('John Doe', 'MALE', 25, 'Club A', 'ADULT'),
+       ('Jane Doe', 'FEMALE', 30, 'Club B', 'ADULT');
+
+
+INSERT INTO discipline (name, description, results_type) VALUES ('Discipline A', 'Description A', 'TIME'),
+                                                              ('Discipline B', 'Description B', 'DISTANCE');
+
+
+INSERT INTO participant_disciplines (participant_id, disciplines_id) VALUES (1, 1),
+                                                                            (1, 2),
+                                                                            (2, 1);
+
+INSERT INTO result (result_date, time_value, distance_value, point_value, participant_id, discipline_id) VALUES ('2022-01-01', '00:10:00', NULL, 10.0, 1, 1),
+                                                                                                                ('2022-01-02', NULL, 100.0, 20.0, 2, 2);
